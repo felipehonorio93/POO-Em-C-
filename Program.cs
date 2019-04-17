8dd370603c05282a54477f1d5e0c7715f2e495e1
Felipe Honorio <32677477+felipehonorio93@users.noreply.github.com>
2019-04-17 14:58:03 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_Bytebank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDoBruno = new ContaCorrente();
+
+            contaDoBruno.titular = "bruno";
+            contaDoBruno.saldo= 150.0;
+            contaDoBruno.Sacar ( 100.0);
+            contaDoBruno.Depositar(100);
+            Console.WriteLine(contaDoBruno.saldo);
+
+
+            ContaCorrente contaGabi = new ContaCorrente();
+            contaGabi.titular = "gabi";
+
+            contaDoBruno.Tranferir(100.0, contaGabi);
+
+            Console.WriteLine("saldo gabi: "+contaGabi.saldo);
+            Console.WriteLine("saldo Bruno: " + contaDoBruno.saldo);
+            Console.ReadLine();
+        }
+    }
+}
