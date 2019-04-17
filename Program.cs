8daa568bd869480894949fcd54f871d9e0ee7eff
Felipe Honorio <32677477+felipehonorio93@users.noreply.github.com>
2019-04-17 14:59:34 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cliente gabriela = new Cliente();
+            gabriela.nome = "Gabriela";
+            gabriela.profissao = "desenvolvedora";
+            gabriela.cpf = "111.455.566-80";
+
+            ContaCorrente conta = new ContaCorrente();
+            conta.titular = gabriela;
+            conta.saldo = 500.0;
+
+            Console.WriteLine(gabriela.nome);
+            Console.WriteLine(conta.titular.nome);
+
+            Console.ReadLine();
+        }
+    }
+}
