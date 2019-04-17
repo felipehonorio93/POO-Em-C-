eaba80d2e89a3e17160984d1f1bf326a8ff5de28
Felipe Honorio <32677477+felipehonorio93@users.noreply.github.com>
2019-04-17 15:03:15 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
+
+            ContaCorrente conta = new ContaCorrente(876, 86712540);
+
+              
+
+
+            Console.WriteLine(conta.Agencia);
+            Console.WriteLine(conta.Numero);
+
+            ContaCorrente contaDaGabriela = new ContaCorrente(867, 86745820);
+
+
+            Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
+
+            Console.ReadLine();
+        }
+    }
+}
