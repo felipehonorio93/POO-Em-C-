f306b0d4dff2848d4c804985175e7f52c7ac2b3b
Felipe Honorio <32677477+felipehonorio93@users.noreply.github.com>
2019-04-17 14:56:18 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,21 +4,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _02_byteBank
+namespace _3_ByteBank
 {
     class Program
     {
         static void Main(string[] args)
         {
-            ContaCorrente conta2 = new ContaCorrente();
+                // exemplo de referencia
+            ContaCorrente contaDaGabriela = new ContaCorrente();
+            contaDaGabriela.titular = "Gabriela";
+            contaDaGabriela.agencia = 863;
+            contaDaGabriela.numero = 863146;
 
-            conta2.titular = "gabriela";
+            ContaCorrente contaDaGabrielaCosta = new ContaCorrente();
+            contaDaGabrielaCosta.titular = "Gabriela";
+            contaDaGabrielaCosta.agencia = 863;
+            contaDaGabrielaCosta.numero = 863146;
 
-            Console.WriteLine(conta2.titular);
-            Console.WriteLine(conta2.saldo);
+            Console.WriteLine("Igualdade de tipo de referência: " + (contaDaGabriela == contaDaGabrielaCosta));
+
+            int idade = 27;
+            int idadeMaisUmaVez = 27;
+
+            Console.WriteLine("Igualdade de tipo de valor: " + (idade == idadeMaisUmaVez));
+
+            contaDaGabriela = contaDaGabrielaCosta;
+            Console.WriteLine(contaDaGabriela == contaDaGabrielaCosta);
+
+            contaDaGabriela.saldo = 300;
+            Console.WriteLine(contaDaGabriela.saldo);
+            Console.WriteLine(contaDaGabrielaCosta.saldo);
+
             Console.ReadLine();
-
-
         }
     }
-}
+    }
+
